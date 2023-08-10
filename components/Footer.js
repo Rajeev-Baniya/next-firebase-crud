487b9713af4e9c23c8d32461ed8b983cf0e62918
rajeev-baniya <rajeevbaniya11@gmail.com>
2023-08-10 12:39:51 +0545

    crud with firebase and next

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center gap-5 py-3">
+      <a href="#">
+        <i className="fa-brands fa-instagram duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+      <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
+      <i className="fa-brands fa-github-alt duration-300 hover:opacity-30 cursor-pointer"></i>
+    </div>
+  );
+};
+
+export default Footer;
